dbb9b2d15cdc1def4b89a313d0c7c0d7efe6a65a
tutich <pkoinange02@gmail.com>
2025-03-11 05:08:30 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        marineBlue: "hsl(213, 96%, 18%)",
+        purplishBlue: "hsl(243, 100%, 62%)",
+        pastelBlue: "hsl(228, 100%, 84%)",
+        lightBlue: "hsl(206, 94%, 87%)",
+        strawberryRed: "hsl(354, 84%, 57%)",
+        coolGray: "hsl(231, 11%, 63%)",
+        lightGray: "hsl(229, 24%, 87%)",
+        magnolia: "hsl(217, 100%, 97%)",
+        alabaster: "hsl(231, 100%, 99%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+
+      fontSize: {
+        sm: "14px",
+        base: "16px",
+        lg: "18px",
+        xl: "20px",
+        "2xl": "24px",
+      },
+
+      fontWeight: {
+        normal: "400",
+        medium: "500",
+        bold: "700",
+      },
+
+      screens: {
+        xs: "320px",
+        sm: "375px",
+        md: "768px",
+        lg: "1024px"
+      }
+    },
+  },
+  plugins: [],
+};
